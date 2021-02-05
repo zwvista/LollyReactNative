5a066c0c9236d016c11a813742fa4beb9a8b3e3c
趙　偉 <zwvista@msn.com>
2021-02-05 13:23:58 +0900

    di

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    "babel-plugin-transform-typescript-metadata",
     ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }]
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
   ],
 };
--- index.js
+++ index.js
@@ -3,8 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import { AppBootstrapper } from 'react-native-modular-bootstrapper';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => AppBootstrapper.startup(App));
+AppRegistry.registerComponent(appName, () => App);
