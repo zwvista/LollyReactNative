2dc1c5c54c43a5b2195554a04860364bde429463
趙偉 <zwvista@msn.com>
2021-03-22 07:32:26 +0900

    service

--- App.tsx
+++ App.tsx
@@ -5,29 +5,6 @@ import { DrawerActions, NavigationContainer, useNavigation } from '@react-naviga
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { HomeScreen, NotificationsScreen } from './screens/Search'
 import { inject } from 'typedin';
-import { WordsUnitService } from './view-models/words-unit.service';
-import { AppService } from './view-models/app.service';
-import { UserSettingService } from './services/misc/user-setting.service';
-import { DictionaryService } from './services/misc/dictionary.service';
-import { TextbookService } from './services/misc/textbook.service';
-import { HtmlService } from './services/misc/html.service';
-import { PhrasesUnitService } from './view-models/phrases-unit.service';
-import { UnitPhraseService } from './services/wpp/unit-phrase.service';
-import { LanguageService } from './services/misc/language.service';
-import { UnitWordService } from './services/wpp/unit-word.service';
-import { SettingsService } from './view-models/settings.service';
-import { AutoCorrectService } from './services/misc/autocorrect.service';
-import { LangPhraseService } from './services/wpp/lang-phrase.service';
-import { LangWordService } from './services/wpp/lang-word.service';
-import { WordsLangService } from './view-models/words-lang.service';
-import { PhrasesLangService } from './view-models/phrases-lang.service';
-import { NoteService } from './view-models/note.service';
-import { VoicesService } from './services/misc/voices.service';
-import { WordFamiService } from './services/wpp/word-fami.service';
-import { WordsFamiService } from './view-models/words-fami.service';
-import { UsMappingService } from './services/misc/us-mapping.service';
-import { PatternService } from './services/wpp/pattern.service';
-import { PatternsService } from './view-models/patterns.service';
 import SettingsScreen from './screens/SettingsScreen';
 
 // https://stackoverflow.com/questions/60316864/react-navigation-drawer-v5
@@ -46,16 +23,6 @@ const DrawerComponent = () => {
   );
 };
 
-@Module({
-  providers: [
-    DictionaryService, HtmlService, LanguageService,
-    TextbookService, UnitPhraseService, UnitWordService, UserSettingService, AppService,
-    PhrasesUnitService, SettingsService, WordsUnitService, AutoCorrectService,
-    LangPhraseService, LangWordService, PhrasesLangService, WordsLangService,
-    NoteService, WordFamiService, WordsFamiService,
-    VoicesService, UsMappingService, PatternService, PatternsService
-  ],
-})
 export default class App extends React.Component<any, any> {
   @inject appService: AppService;
 
