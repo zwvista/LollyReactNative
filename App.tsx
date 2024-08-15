59b7dbe37fd66f8a85ab264b8a71023e23b42ce2
趙偉 <zwvista@msn.com>
2024-08-15 15:21:19 +0900

    logout

--- App.tsx
+++ App.tsx
@@ -12,6 +12,7 @@ import {createDrawerNavigator} from "@react-navigation/drawer";
 import {HomeScreen, NotificationsScreen} from "./screens/Search";
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { DrawerActions, NavigationContainer, useNavigation } from '@react-navigation/native';
+import { Button } from "react-native";
 
 
 const Drawer = createDrawerNavigator();
@@ -19,7 +20,10 @@ const Drawer = createDrawerNavigator();
 const DrawerComponent = () => {
   return (
     <Drawer.Navigator initialRouteName="Home">
-      <Drawer.Screen name="Home" component={HomeScreen} options={{drawerIcon: () => <Ionicons name='home' size={30} color='#130f40' />}}/>
+      <Drawer.Screen name="Home" component={HomeScreen} options={{
+        headerRight: () => <Button title="Logout" />,
+        drawerIcon: () => <Ionicons name='home' size={30} color='#130f40' />
+      }}/>
       <Drawer.Screen name="Notifications" component={NotificationsScreen} options={{drawerIcon: () => <Ionicons name='notifications' size={30} color='#130f40' />}}/>
     </Drawer.Navigator>
   );
--- screens/Search.tsx
+++ screens/Search.tsx
@@ -3,13 +3,26 @@ import { Button, View } from 'react-native';
 import { container } from "tsyringe";
 import { AppService } from "../view-models/misc/app.service.ts";
 import { Login } from "./Login.tsx";
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { GlobalVars } from "../common/common.ts";
 
 export function HomeScreen({ navigation }:any) {
   const appService = container.resolve(AppService);
   const [showLogin, setShowLogin] = useState(false);
+  const [loginCount, updateLoginCount] = useReducer(x => x + 1, 0);
+
+  const logout = async () => {
+    await AsyncStorage.removeItem('userid');
+    GlobalVars.userid = "";
+    updateLoginCount();
+  };
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <Button onPress={logout} title="Logout" />
+    });
+  }, []);
 
   useEffect(() => {
     (async () => {
@@ -20,7 +33,7 @@ export function HomeScreen({ navigation }:any) {
         GlobalVars.userid = loggedIn!;
       }
     })();
-  }, []);
+  }, [loginCount]);
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
