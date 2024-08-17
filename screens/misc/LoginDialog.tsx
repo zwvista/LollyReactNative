c6bc7c3d434b8195d60b52d0c0967dc9a2f3f9fe
趙偉 <zwvista@msn.com>
2024-08-18 04:19:24 +0900

    formik

--- screens/misc/LoginDialog.tsx
+++ screens/misc/LoginDialog.tsx
@@ -6,64 +6,60 @@ import { LoginService } from "../../view-models/misc/login.service.ts";
 import { useMMKVStorage } from "react-native-mmkv-storage";
 import { storage, stylesApp } from "../../App.tsx";
 import Modal from "react-native-modal";
+import { Formik, useFormik } from "formik";
+import { MUser } from "../../models/misc/user.ts";
 
 export default function LoginDialog(
   {isDialogOpened, handleCloseDialog}: {isDialogOpened: boolean, handleCloseDialog: () => void}
 ) {
   const loginService = container.resolve(LoginService);
-  const [, forceUpdate] = useReducer(x => x + 1, 0);
   const [, setUserid] = useMMKVStorage('userid', storage, '');
 
-  const onChangeUsername = (e: string) => {
-    loginService.item.USERNAME = e;
-    forceUpdate();
-  };
-
-  const onChangePassword = (e: string) => {
-    loginService.item.PASSWORD = e;
-    forceUpdate();
-  };
-
-  const login = async () => {
-    const userid = await loginService.login();
-    if (userid) {
-      setUserid(userid);
-      handleCloseDialog();
+  const formik = useFormik({
+    initialValues: loginService.item,
+    onSubmit: async (values: MUser) => {
+      loginService.item = values;
+      const userid = await loginService.login();
+      if (userid) {
+        setUserid(userid);
+        handleCloseDialog();
+      }
     }
-  };
+  });
 
   return (
     <Modal isVisible={isDialogOpened}>
       <TouchableNativeFeedback onPress={Keyboard.dismiss}>
-        <SafeAreaView style={{flex: 1, alignItems: 'center', justifyContent: 'center', padding: 8, backgroundColor: 'white'}}>
-          <View style={{flexDirection: "row", alignItems: "center"}}>
-            <View style={{width: '30%'}}>
-              <Text>USERNAME:</Text>
+        <Formik initialValues={loginService.item} onSubmit={() => formik.handleSubmit()}>
+          <SafeAreaView style={{flex: 1, alignItems: 'center', justifyContent: 'center', padding: 8, backgroundColor: 'white'}}>
+            <View style={{flexDirection: "row", alignItems: "center"}}>
+              <View style={{width: '30%'}}>
+                <Text>USERNAME:</Text>
+              </View>
+              <View style={{width: '70%'}}>
+                <TextInput
+                  style={stylesApp.textinput}
+                  {...formik.getFieldProps('USERNAME')}
+                />
+              </View>
             </View>
-            <View style={{width: '70%'}}>
-              <TextInput
-                style={stylesApp.textinput}
-                value={loginService.item.USERNAME}
-                onChangeText={onChangeUsername}
-              />
+            <View style={{flexDirection: "row", alignItems: "center"}}>
+              <View style={{width: '30%'}}>
+                <Text>PASSWORD:</Text>
+              </View>
+              <View style={{width: '70%'}}>
+                <TextInput
+                  style={stylesApp.textinput}
+                  secureTextEntry
+                  {...formik.getFieldProps('PASSWORD')}
+                />
+              </View>
             </View>
-          </View>
-          <View style={{flexDirection: "row", alignItems: "center"}}>
-            <View style={{width: '30%'}}>
-              <Text>PASSWORD:</Text>
+            <View style={{flexDirection: "row", justifyContent: "center"}}>
+              <Button title="Login" onPress={() => formik.handleSubmit()} />
             </View>
-            <View style={{width: '70%'}}>
-              <TextInput
-                style={stylesApp.textinput}
-                value={loginService.item.PASSWORD}
-                onChangeText={onChangePassword}
-              />
-            </View>
-          </View>
-          <View style={{flexDirection: "row", justifyContent: "center"}}>
-            <Button title="Login" onPress={login} />
-          </View>
-        </SafeAreaView>
+          </SafeAreaView>
+        </Formik>
       </TouchableNativeFeedback>
     </Modal>
   );
