e9ddba1bf27b3ef22eb419442bf70d85ddaeec2d
趙偉 <zwvista@msn.com>
2024-08-18 04:47:24 +0900

    formik 2

--- screens/misc/LoginDialog.tsx
+++ screens/misc/LoginDialog.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import { Button, Keyboard, SafeAreaView, Text, TextInput, TouchableNativeFeedback, View } from 'react-native';
 import { container } from "tsyringe";
-import { useReducer } from "react";
 import { LoginService } from "../../view-models/misc/login.service.ts";
 import { useMMKVStorage } from "react-native-mmkv-storage";
 import { storage, stylesApp } from "../../App.tsx";
 import Modal from "react-native-modal";
-import { Formik, useFormik } from "formik";
+import { Formik } from "formik";
 import { MUser } from "../../models/misc/user.ts";
+import { FormikHelpers } from "formik/dist/types";
 
 export default function LoginDialog(
   {isDialogOpened, handleCloseDialog}: {isDialogOpened: boolean, handleCloseDialog: () => void}
@@ -15,52 +15,56 @@ export default function LoginDialog(
   const loginService = container.resolve(LoginService);
   const [, setUserid] = useMMKVStorage('userid', storage, '');
 
-  const formik = useFormik({
-    initialValues: loginService.item,
-    onSubmit: async (values: MUser) => {
-      loginService.item = values;
-      const userid = await loginService.login();
-      if (userid) {
-        setUserid(userid);
-        handleCloseDialog();
-      }
+  const login = async (values: MUser, formikHelpers: FormikHelpers<MUser>) => {
+    loginService.item = values;
+    const userid = await loginService.login();
+    if (userid) {
+      setUserid(userid);
+      handleCloseDialog();
     }
-  });
+  };
 
   return (
     <Modal isVisible={isDialogOpened}>
-      <TouchableNativeFeedback onPress={Keyboard.dismiss}>
-        <Formik initialValues={loginService.item} onSubmit={() => formik.handleSubmit()}>
-          <SafeAreaView style={{flex: 1, alignItems: 'center', justifyContent: 'center', padding: 8, backgroundColor: 'white'}}>
-            <View style={{flexDirection: "row", alignItems: "center"}}>
-              <View style={{width: '30%'}}>
-                <Text>USERNAME:</Text>
+      <Formik
+        initialValues={loginService.item}
+        onSubmit={login}
+      >
+        {({handleChange, handleSubmit, values}) =>
+          <TouchableNativeFeedback onPress={Keyboard.dismiss}>
+            <SafeAreaView style={{alignItems: 'center', justifyContent: 'center', padding: 8, backgroundColor: 'white'}}>
+              <View style={{flexDirection: "row", alignItems: "center"}}>
+                <View style={{width: '30%'}}>
+                  <Text>USERNAME:</Text>
+                </View>
+                <View style={{width: '70%'}}>
+                  <TextInput
+                    style={stylesApp.textinput}
+                    value={values.USERNAME}
+                    onChangeText={handleChange('USERNAME')}
+                  />
+                </View>
               </View>
-              <View style={{width: '70%'}}>
-                <TextInput
-                  style={stylesApp.textinput}
-                  {...formik.getFieldProps('USERNAME')}
-                />
+              <View style={{flexDirection: "row", alignItems: "center"}}>
+                <View style={{width: '30%'}}>
+                  <Text>PASSWORD:</Text>
+                </View>
+                <View style={{width: '70%'}}>
+                  <TextInput
+                    style={stylesApp.textinput}
+                    secureTextEntry
+                    value={values.PASSWORD}
+                    onChangeText={handleChange('PASSWORD')}
+                  />
+                </View>
               </View>
-            </View>
-            <View style={{flexDirection: "row", alignItems: "center"}}>
-              <View style={{width: '30%'}}>
-                <Text>PASSWORD:</Text>
+              <View style={{flexDirection: "row", justifyContent: "center"}}>
+                <Button title="Login" onPress={() => handleSubmit()} />
               </View>
-              <View style={{width: '70%'}}>
-                <TextInput
-                  style={stylesApp.textinput}
-                  secureTextEntry
-                  {...formik.getFieldProps('PASSWORD')}
-                />
-              </View>
-            </View>
-            <View style={{flexDirection: "row", justifyContent: "center"}}>
-              <Button title="Login" onPress={() => formik.handleSubmit()} />
-            </View>
-          </SafeAreaView>
-        </Formik>
-      </TouchableNativeFeedback>
+            </SafeAreaView>
+          </TouchableNativeFeedback>
+        }
+      </Formik>
     </Modal>
   );
 }
