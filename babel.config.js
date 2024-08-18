090eabbc585f9821bf4d4696f33af51eaa7db29c
趙偉 <zwvista@msn.com>
2024-08-18 14:13:10 +0900

    nativewind

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,7 @@ module.exports = {
     'babel-plugin-transform-typescript-metadata',
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
+    "nativewind/babel",
     'react-native-reanimated/plugin',
   ],
 };
